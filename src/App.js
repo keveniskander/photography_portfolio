81ad533a288f49945a2ce347763474480da03e57
keveniskander <keven.iskander@hotmail.com>
2022-02-10 23:41:29 -0500

    got it to work. was using routing v5 syntax should've been using routing v6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import logo from './logo.svg';
 // Import the BrowserRouter, Route and Link components
-import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import Projects from './Projects.js'; 
 import Travel from './Travel.js'; 
 import About from './About.js'; 
@@ -12,25 +12,9 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-  
-        {/* Set up the Router */}
-        <Route exact path="/" component={Projects} />
-        <Route path="/travel" component={Travel} />
-        <Route path="/about" component={About} />
-  
-        <div className="navigation">
-          <img src={logo} className="logo-image" alt="Logo Image" />
-          <div className="navigation-sub">
-                                          
-            // Set up the Links
-            <Link to="/" className="item">Projects</Link>
-            <Link to="/travel" className="item">Travel</Link>
-            <Link to="/about" className="item">About</Link>
-  
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Projects />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,10 @@ function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
+      risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
+      nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
+      lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.</p>
     </div>
   )
 }
