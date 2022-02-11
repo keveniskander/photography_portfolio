b62e6939be817637909e1f73b3cfb04e0dbf5560
keveniskander <keven.iskander@hotmail.com>
2022-02-11 00:51:48 -0500

    links working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import logo from './logo.svg';
 // Import the BrowserRouter, Route and Link components
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'; 
 import Projects from './Projects.js'; 
 import Travel from './Travel.js'; 
 import About from './About.js'; 
@@ -11,11 +11,30 @@ import './App.css';
  
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <body>
+      <section className='home'>
+        <BrowserRouter>
+          <nav style={{ margin: 10 }}>
+          <Link to="/" style={{ padding: 5 }}>
+            Project
+          </Link>
+          <Link to="/about" style={{ padding: 5 }}>
+            About
+          </Link>
+          <Link to="/travel" style={{ padding: 5}}>
+            Travel
+          </Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Projects />} />
+            <Route path="/travel" element={<Travel />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+
+        </BrowserRouter>
+      </section>
+    </body>
+    
   );
 }
  
