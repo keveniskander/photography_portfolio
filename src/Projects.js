d3b237f0085287d8a5ec9df9580d9d8f49611543
keveniskander <keven.iskander@hotmail.com>
2022-03-03 21:59:43 -0500

    clean code

--- src/Projects.js
+++ src/Projects.js
@@ -1,39 +1,48 @@
 import React from "react"
-import background from '../src/images/ROM_copy.jpg'
+import background from '../src/images/ROM_copy_BW.jpg'
  
 function Projects(props) {
   return (
         <section class="main-section">
           <div class="main-div">
             <img class="background" src={background}/>
-            <div class="col-12 text-center">
-                <h1>ByKevo</h1>
-            </div>
-            <h1>Projects</h1>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
-            risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
-            nulla interdum justo. Pellentesque dignissim, sapien et congue rutrum,
-            lorem tortor dapibus turpis, sit amet vestibulum eros mi et odio.</p>
+            
+          
+
+            <div class="h1-container">
+              <h1>ByKevo</h1>
             </div>
             <div class="h1-container">
               <h1>collection</h1>
             </div>
+
             <div class="h2-container">
               <h2>OTB</h2>
             </div>
-          <div class="portfolio-container">
-            <div class="p-container">
-              <p class="p1">latest</p>
+
+            <div class="col-12 text-center mt-4 mt-lg-5">
+              <p>
+                <span class="travel hover-target">travel</span> 
+                <span class="wildlife hover-target">wildlife</span> 
+                <span class="nature hover-target">nature</span>
+              </p>
             </div>
-            <div class="photography-portfolio">
-              <img src="" class="img-left"/>
-              <img src="" class="img-right"/>
-              <img src="" class="img-left"/>
-              <img src="" class="img-right"/>
-              <img src="" class="img-left"/>
-              <img src="" class="img-right"/>
+            <div class="portfolio-container">
+              <div class="p-container">
+                <p class="p1">latest</p>
+              </div>
+              <div class="photography-portfolio">
+                <img src="" class="img-left"/>
+                <img src="" class="img-right"/>
+                <img src="" class="img-left"/>
+                <img src="" class="img-right"/>
+                <img src="" class="img-left"/>
+                <img src="" class="img-right"/>
+              </div>
             </div>
           </div>
+          
+          
         </section>
     
   )
